10f7d9c9020cb35f26bd269a6d9696ff0090d17e
Лазаренко Дмитрий <d.lazarenko@solarlab.ru>
2024-04-24 10:12:58 +0300

    Добавлен API-тест: получение списка категорий

--- src/Hosts/SolarLab.Academy.Api/Program.cs
+++ src/Hosts/SolarLab.Academy.Api/Program.cs
@@ -65,3 +65,5 @@ app.UseSwaggerUI();
 app.MapControllers();
 
 app.Run();
+
+public partial class Program {}
---
+++ tests/SolarLab.Academy.ApiTests/WebApplicationFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using SolarLab.Academy.AppServices.Categories.Repositories;
+
+namespace SolarLab.Academy.ApiTests
+{
+    public class WebApplicationFactory : WebApplicationFactory<Program>
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped<ICategoryRepository, CategoryRepositoryStub>();
+                services.AddScoped<IDistributedCache, MemoryDistributedCache>();
+            });
+        }
+    }
+}
